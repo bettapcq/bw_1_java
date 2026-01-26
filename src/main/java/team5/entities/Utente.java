b39433714ad9e5e84d9ef689f3ff994ad2e60209
Mattia Zerbini <zermatty6@gmail.com>
2026-01-26 16:42:17 +0100

    Add class Utente and Biglietto

--- src/main/java/team5/entities/Utente.java
+++ src/main/java/team5/entities/Utente.java
@@ -1,4 +1,73 @@
 package team5.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "utenti")
 public class Utente {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_utente")
+    private UUID idUtente;
+
+    @Column(name = "nome", length = 30)
+    String nome;
+
+    @Column(name = "cognome", length = 30)
+    String cognome;
+
+    @Column(name = "data_nascita")
+    LocalDate data_nascita;
+
+    public Utente(){
+    }
+
+    public Utente(String nome, String cognome, LocalDate data_nascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.data_nascita = data_nascita;
+    }
+
+    public UUID getIdUtente() {
+        return idUtente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public LocalDate getData_nascita() {
+        return data_nascita;
+    }
+
+    public void setData_nascita(LocalDate data_nascita) {
+        this.data_nascita = data_nascita;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "idUtente=" + idUtente +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", data_nascita=" + data_nascita +
+                '}';
+    }
 }
+
