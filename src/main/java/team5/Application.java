7dd2e2994e67bfa1763227e94df4720f7097d1d4
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-01-27 12:36:55 +0100

    fix

--- src/main/java/team5/Application.java
+++ src/main/java/team5/Application.java
@@ -3,18 +3,13 @@ package team5;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import team5.dao.AbbonamentiDAO;
-import team5.dao.TessereDAO;
+
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw1javapu");
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
         System.out.println("Hello World!1 ");
-
-        AbbonamentiDAO ad = new AbbonamentiDAO(em);
-        TessereDAO td = new TessereDAO(em);
-
         em.close();
         emf.close();
 
